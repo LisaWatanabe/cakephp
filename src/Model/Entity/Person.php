35abbd27806d903b7e19ccdbf049131275b26dd3
LisaWatanabe <risawatanabe91@gmail.com>
2019-08-30 12:44:05 +0900

    First Commit!

---
+++ src/Model/Entity/Person.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Model\Entity;
+use Cake\ORM\Entity;
+
+class Person extends Entity {
+	protected $_accessible = [
+		'*' => true,
+		'id' => false];
+}
+
+
+
+
+
+
+
+
+
+
+  ?>
